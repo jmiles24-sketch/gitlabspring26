201bbe0a64e0e4f62d4b66edad2cd868d8b399e5
jmiles24-sketch <jmiles24@toromail.csudh.edu>
2026-04-28 03:35:59 +0000

    Added function to print my initials

---
+++ name.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void printInitials_JM();
+int main() {
+    printInitials_JM();
+    
+    return 0;
+}
+
+void printInitials_JM() {
+    printf("My initials are: J. M. (Jayden Miles)\n");
+}
